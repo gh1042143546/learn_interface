742e6e18f53b44956356eae36516ce6e18c689e2
gh1042143546 <1042143546@qq.com>
2019-01-22 00:05:03 +0800

    更新client

--- utils/readCase.py
+++ utils/readCase.py
@@ -6,7 +6,10 @@ class read_params:
         #print(reader.read_case())
     def get_params(self):
         params = self.reader.read_case()
+        for i in params:
+            print(i)
+            #print(i['denglu'][0]['method'])
         print(params)
 if __name__== '__main__':
-    b = read_params('/Users/lyg/PycharmProjects/learn_interface/date/cases')
+    b = read_params('../date/cases')
     b.get_params()
