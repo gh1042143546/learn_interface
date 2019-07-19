baa198efd1a7fb5954fe0e638dc73365930b3b5a
lyg <lyg@LYGdeMacBook-Air.local>
2019-07-19 18:56:02 +0800

    数据结构

--- test/case/qwer.py
+++ test/case/qwer.py
@@ -1,5 +1,15 @@
 # -*- coding:utf-8 -*-
 # Author:Gehao
-a=[[]]
-a[0].append("a")
-print(a)
+from deptest import depend_on
+
+@depend_on('test_b',with_return=True)
+def test_a(name):
+    print("abc"+name)
+
+def test_b():
+    #print("dfg")
+    name ="def"
+    return name
+
+
+test_a()
--- test/case/test_baidu.py
+++ test/case/test_baidu.py
@@ -3,7 +3,6 @@ import allure
 from utils.assertion import assertHTTPCode
 from utils.client import *
 from utils.readCase import read_params
-from utils.config import Config
 '''
 #@allure.feature("测试接口")
 @pytest.fixture(params=['base_url'])
@@ -21,21 +20,15 @@ def readInfo(request):
 
 class TestBaiDuHTTP:
     readyaml = read_params(r'../../date/cases/denglu.yaml')
-
     paramdate = readyaml.get_params()
-
-    #@pytest.mark.parametrize("url,method,date,check",paramdate,ids=['正常登录','秘密错误'])
-    #test_case_01为用例title
-    #@allure.feature('登录模块')
     @allure.feature('登录模块')
     @allure.story('登录接口')
     @allure.issue("BUG号：123") # 问题表识，关联标识已有的问题，可为一个url链接地址
     @allure.testcase("用例名：登录校验")
-    #@allure.Title("冒烟测试_所有算子运行_正常测试")
-    @pytest.mark.parametrize("url,method,date,check",paramdate)
+    @allure.title("冒烟测试_所有算子运行_正常测试")
+    @pytest.mark.parametrize("url,method,date,check",paramdate,ids=["正常登录","异常登录"])
     # ids，对应用例参数化数据的用例名
 
-    #@allure.description("测试一个流程，用作回归冒烟测试")
     def test_case(self,url,method,date,check):
         """用例描述：登录验证
         URL:参数一
@@ -49,10 +42,10 @@ class TestBaiDuHTTP:
         response = c.sendRequest(param=date)
         logger.info(response.text)
         b = response.json()
-        #assert b["code"] == check["code"]
-            # 测试步骤，对必要的测试过程加以说明
-        with pytest.allure.step("获取登录接口返回值，结果校验 {0} == {1}".format(b["code"], check["code"])):
-            assert b["code"] == check["code"]
+        # 测试步骤，对必要的测试过程加以说明
+        with allure.step("获取登录接口返回值，结果校验 {0} == {1}".format(b["code"], check["code"])):
+            #assert b["code"] == check["code"],"判断返回码，当前code的值为：%s"%b["code"]
+            assert "access_token" in b['data'].keys()
 
 
 if __name__ == '__main__':
