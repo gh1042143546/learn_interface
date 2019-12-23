a76b7a1850cea6b51861ce543f4941e61b1f07f3
frank <1042143546@qq.com>
2019-12-24 00:08:22 +0800

    juu

--- utils/file_readers.py
+++ utils/file_readers.py
@@ -4,9 +4,10 @@ import yaml
 import os
 import re
 from utils import support
+
 class YamlReader:
     parser = GooeyParser(description="My Cool Gooey App!")
-    parser.add_argument('filename', help="name of the file to process", widget='FileChooser')
+    parser.add_argument('yamlfilePath', help="name of the file to process", widget='FileChooser')
     def __init__(self,yamlfilePath):
         if os.path.exists(yamlfilePath):
             self.yamlfilePath = yamlfilePath
@@ -22,9 +23,9 @@ class YamlReader:
         file = open(self.yamlfilePath,encoding='UTF-8')
         casedate = yaml.load(file,Loader=yaml.FullLoader)
         return casedate
-
+#这里没有
 if __name__== '__main__':
     path_dir = os.path.split(os.path.realpath(__file__))[0]#项目目录
     reader = YamlReader('../date/cases/denglu.yaml')
     b = reader.read_case()
-    print(b)
+    print(b)#输出
